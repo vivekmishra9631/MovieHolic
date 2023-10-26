776a173c07f17c045970ffc17a1e6e8d9b11295f
vivekmishra9631 <vivekmishra.211ch059@gmail.com>
2023-10-26 11:36:27 +0530

    added src files

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
+import { useEffect } from 'react'
+import { useGlobalState } from './store'
+import { isWallectConnected, loadNfts } from './MovieHolic'
+
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Artworks from './components/Artworks'
+import Footer from './components/Footer'
+import Alert from './components/Alert'
+import Loading from './components/Loading'
+
 const App = () => {
+
+  const [nfts] = useGlobalState('nfts')
+
+  useEffect(async () => {
+    await isWallectConnected().then(() => console.log('Blockchain Loaded'))
+    await loadNfts()
+  }, [])
+ 
   return (
     <div className="min-h-screen">
-      <h4 className="text-3xl">Hello Hardhat and Tailwind</h4>
+      <div className="gradient-bg-hero">
+        <Header/>
+        <Hero/>
+      </div>
+      <Artworks artworks={nfts} />
+      <Footer />
+      <Alert/>
+      <Loading/>
+
     </div>
   )
 }
